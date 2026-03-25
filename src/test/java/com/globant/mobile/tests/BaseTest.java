da6459ca5db6086227698e3fc350457a63c66b09
Jeronimo Acosta  Acevedo <jeronimo.acosta@globant.com>
2026-03-25 08:44:43 -0500

    Add setup BeforeMethod function

--- src/test/java/com/globant/mobile/tests/BaseTest.java
+++ src/test/java/com/globant/mobile/tests/BaseTest.java
@@ -1,4 +1,41 @@
 package com.globant.mobile.tests;
 
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BaseTest {
+
+    @BeforeMethod
+    public AppiumDriver setup() {
+
+        try {
+            //Set up the exact same capabilities we used in the Inspector
+            UiAutomator2Options options = new UiAutomator2Options()
+                    .setPlatformName("Android")
+                    .setDeviceName("emulator-5554")
+                    .setAppPackage("com.android.settings")
+                    .setAppActivity(".Settings"); //Make sure to change this depending on the main activity of the app.
+
+            URL url = new URL("http://127.0.0.1:4723");
+            AppiumDriver appium_driver = new AppiumDriver(url,options);
+
+            appium_driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            return appium_driver;
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
